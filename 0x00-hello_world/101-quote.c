e0e1c1c6691a1e3a38766ededfd2da674c6f0c76
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-02 10:46:42 -0700

    Bug fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry Point
  *
@@ -10,6 +11,6 @@
 
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n", 59);
 return (1);
 }
