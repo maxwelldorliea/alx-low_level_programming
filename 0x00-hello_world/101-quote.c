3c3e6d8779dc2ba69316c3bfc7bba5f166ca87ef
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-02 10:15:10 -0700

    Added file 101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: prints a string to the standard error
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
