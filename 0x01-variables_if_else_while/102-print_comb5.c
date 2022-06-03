efeb83a55212727fcc743b30d46208816d46b1cb
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-03 14:15:55 -0700

    Error fixef

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,22 +14,20 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j < 9; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			for (k = j + 1; k < 10; k++)
-			{
-				for (l = k + 1; l < 10; l++)
-				{
-					putchar('0' + i % 10);
-					putchar('0' + j % 10);
-					putchar(',');
-					putchar(' ');
-					if (k == 9 && l == 9)
-						break;
-					putchar('0' + k % 10);
-					putchar('0' + l % 10);
-				}
-			}
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+
+			putchar(' ');
+
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
