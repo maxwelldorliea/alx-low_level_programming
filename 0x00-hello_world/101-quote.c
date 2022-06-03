126c52aa938d4a14afd34871964b70a2c0ea16fc
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-02 17:27:55 -0700

    Error fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry Point
  *
@@ -10,6 +11,6 @@
 int main(void)
 {
 char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
-fprintf(stderr,  "%s", err);
+write(2, err, 59);
 return (1);
 }
