c2b62a221c8fa5bdedab736f4d7bb3aa3cc317fe
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-02 17:38:05 -0700

    fixed error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199";
 write(2, err, sizeof(err));
 return (1);
 }
