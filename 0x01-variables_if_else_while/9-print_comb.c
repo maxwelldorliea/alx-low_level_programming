16b6783f903019e717b12441789df691fcafb0a0
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-03 12:51:31 -0700

    Added 9 print comb file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Entry Point
+ *
+ * Description: print all combinations of single digit
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i % 10);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
