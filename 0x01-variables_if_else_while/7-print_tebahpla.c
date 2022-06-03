781ff97ca8cf3c4a38c4677c8632ab386017be63
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-03 11:37:05 -0700

    Added 7 print lebahpla file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print all alpha in reverse order
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	char alph[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 26; i >= 0; i--)
+		putchar(alph[i]);
+	putchar('\n');
+	return (0);
+}
