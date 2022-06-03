446d78bc9dd90933d3826fb8fa8ab2fb68e7444f
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-03 09:21:52 -0700

    Added 2 print alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: prints of alphabets in lowercase with putschar function
+ *
+ * Return: Always 0(Success)
+ */
+
+
+int main(void)
+{
+	char alpha[] = "abcdefhhijklmnopqrstuvwxzy";
+
+	for (int i = 0; i < 26; i++)
+		putchar(alpha[i]);
+	putchar('\n');
+	return (0);
+}
+
