c9c1f1125ef623d0045457e95382e21d3c784847
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-07 15:46:10 -0700

    Error fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,14 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 			printf("%d, ", n);
 		printf("%d\n", 98);
 	}
 
 	else
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 			printf("%d, ", n);
 		printf("%d\n", 98);
 	}
