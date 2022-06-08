456830f9b78c70a0e2a76732805bbb701a84e93d
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-07 17:03:36 -0700

    Added file 102 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 50 fibo atarting with 1,2
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int c = 50, n1 = 1, n2 = 2, prevS = 0, i;
+
+	for (i = 1; i <= c; i++)
+	{
+		printf("%d ", n1);
+
+		prevS = n1 + n2;
+		n1 = n2;
+		n2 = prevS;
+	}
+
+	return (0);
+}
