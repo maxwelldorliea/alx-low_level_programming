353e94e15fb2ae8bae561dfaa9a031a929033aed
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-07 17:34:45 -0700

    Bug fixed

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,13 +8,15 @@
 
 int main(void)
 {
-	int c = 10, n1 = 1, n2 = 2, prevS = 0, sum = 0, i;
+	long int c = 10, n1 = 1, n2 = 2, prevS = 0, sum = 0;
 
-	for (i = 1; i <= c; i++)
+	while (1)
 	{
+		prevS = n1 + n2;
+		if (prevS > 4000000)
+			break;
 		if (n1 % 2 == 0)
 			sum += n1;
-		prevS = n1 + n2;
 		n1 = n2;
 		n2 = prevS;
 	}
