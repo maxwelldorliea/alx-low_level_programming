4fef1771e645023a3227341104fd60d375159053
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-07 17:17:04 -0700

    Bug fixed

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,7 +12,12 @@ int main(void)
 
 	for (i = 1; i <= c; i++)
 	{
-		printf("%ld ", n1);
+		printf("%ld", n1);
+		if (i == 50)
+		{
+			printf("\n");
+			break;
+		printf(", ");
 
 		prevS = n1 + n2;
 		n1 = n2;
