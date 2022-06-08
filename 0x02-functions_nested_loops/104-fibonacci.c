a032cf4da8724ed81d9cf77dc57fb4a98a777dbe
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-07 17:50:02 -0700

    Added 104 fibonacci file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 98 fibo starting with 1,2
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int c = 98, n1 = 1, n2 = 2, prevS = 0, i;
+
+	for (i = 1; i <= c; i++)
+	{
+		printf("%ld", n1);
+
+		if (i == 98)
+		{
+			printf("\n");
+			break;
+		}
+	}
+
+	printf(", ");
+
+	return (0);
+}
