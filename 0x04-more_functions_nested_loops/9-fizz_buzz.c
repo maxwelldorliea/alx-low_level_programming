6c03ec36c62c7bdb7d7ca8b645127e47b4c87b79
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-09 07:33:54 -0700

    Added 9 fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print from 1 to 10, multiply of 3, Fizz, 5, Buzz, both FizzBuzz
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+
+		else if (i % 5 == 0)
+			printf("Buzz");
+
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+		if (i == 100)
+			break;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
