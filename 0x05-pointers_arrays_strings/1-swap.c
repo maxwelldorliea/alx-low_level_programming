fae106d69c506f21adcff2311079abfbf25e75b6
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-12 22:25:38 -0700

    Added file 1 swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * swap_int - swap to integer values
+ *
+ * @a: int
+ *
+ * @b: int
+ *
+ * Return: Nothing
+ */
+
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+
+	*b = tmp
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,4 +2,8 @@
 
 void reset_to_98(int *n);
 
+void swap_int(int *a, int *b);
+
+
+
 
