88a680078cdbdda0be1977345e8d0e8490c7aa59
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-12 22:18:11 -0700

    Added file 0 reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - change the value of an integer to 98
+ *
+ * @n: int pointer
+ *
+ * Return: Nothing
+ */
+
+
+
+void reset_to_98(int *n)
+{
+	n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+
+
+void reset_to_98(int *n);
+
+
