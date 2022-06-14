0881c0726d15d04ba69f0f9110757ce6e0ee48b8
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-13 19:34:09 -0700

    Bug fixed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,19 +11,24 @@
 
 int _atoi(char *s)
 {
-	int i = 0, sum = 0;
+	int i;
+	int res = 0;
+	int sig = -1;
+	int brk = 0;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] < 48 || s[i] > 57)
-			continue;
-		else if (s[i] >= 48 || s[i] <= 57)
-			sum = sum * 10 + (s[i] - 48);
-		i++;
+		if (s[i] == '-')
+			sig = sig * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10;
+			res -= (s[i] - '0');
+			brk = 1;
+		}
+		else if (brk == 1)
+			break;
 	}
-
-	if (s[0] == '-')
-		return (-1 * sum);
-
-	return (sum);
+	res = sig * res;
+	return (res);
 }
