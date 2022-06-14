37d82058b2470ebee7baf0e3b685ebeb856edc9d
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-13 19:07:22 -0700

    Added 100 atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,7 +18,7 @@ int _atoi(char *s)
 		if (s[i] < 48 || s[i] > 57)
 			return (0);
 		else if (s[i] >= 48 || s[i] <= 57)
-			sum = sum * 10 + (str[i] - 48);
+			sum = sum * 10 + (s[i] - 48);
 		i++;
 	}
 
