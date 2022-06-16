d371be5c395affd587c9d0c305653ab3c2db450e
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-16 12:49:05 -0700

    Added file 6 cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * cap_string - captialized every char at 0 index and char after a searator
+ *
+ * @s: char pointer
+ * Return: char pointer
+ */
+
+
+char *cap_string(char *s)
+{
+
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while (!(s[i] >='a' && s[i] <= 'z'))
+			i++;
+		if (s[i - 1] == ',' ||
+		    s[i - 1] == ';' ||
+		    s[i - 1] == '.' ||
+		    s[i - 1] == '!' ||
+		    s[i - 1] == '?' ||
+		    s[i - 1] == ' ' ||
+		    s[i - 1] == '"' ||
+		    s[i - 1] == '\n' ||
+		    s[i - 1] == '\t' ||
+		    s[i - 1] == '(' ||
+		    s[i - 1] == '}' ||
+		    s[i - 1] == '{' ||
+		    s[i - 1] == ')' ||
+		    i == 0)
+			s[i] -= 32;
+
+		i++;
+	}
+
+
+	return (s);
+}
+
