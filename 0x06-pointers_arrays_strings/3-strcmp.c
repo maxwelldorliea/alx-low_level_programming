e7c6161c67f10039b81298cd0a0ebc993d5a3a02
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-16 04:23:41 -0700

    Bug fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * get_len - returns the length of a string
- *
- * @str: char pointer
- *
- * Return: int
- */
-
-int get_len(char *str)
-{
-	int len = 0;
-
-	while (*str != '\0')
-	{
-		len++;
-		str++;
-	}
-
-	return (len);
-}
-
-/**
  * _strcmp - compare two string, return 1 if s1 > s2, 0 when s1 =s2, -1 s1 < s2
  *
  * @s1: char pointer
@@ -33,25 +12,11 @@ int get_len(char *str)
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1 = get_len(s1);
-	int len2 = get_len(s2);
-	int i = 0;
-
-	#define TRUE 1
-	#define FALSE -1
-	#define EQ 0
-	#define LNE 32
-
-	if (len1 != len2)
-		return (LNE);
-	while (s1[i] != '\0' && i < len1)
+	while (*s1 && (*s1 == *s2))
 	{
-		if (s1[i] > s2[i])
-			return (TRUE);
-		else if (s1[i] < s2[i])
-			return (FALSE);
-		i++;
+		s1++;
+		s2++;
 	}
 
-	return (-EQ);
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
 }
