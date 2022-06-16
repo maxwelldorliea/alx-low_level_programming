9bc18c030d474bb48e1d0c364d2d11e054041d82
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-16 03:20:34 -0700

    Bug fixed

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * get_len - returns the length of a string
- *
- * @str: char pointer
- *
- * Return: int
- */
-
-/*int get_len(char *str)
-{
-	int len = 0;
-
-	while (*str != '\0')
-	{
-		len++;
-		str++;
-	}
-
-	return (len);
-}*/
-
-/**
  * _strncpy - Copy n bites of src to dest
  *
  * @dest: char pointer
