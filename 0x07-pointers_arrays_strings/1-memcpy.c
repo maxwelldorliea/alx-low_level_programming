a1a68dacca9d2b36e389396eb8e12a012e54f1f0
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-20 10:48:12 -0700

    Bug fixed

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * get_len - return the length of a string
- *
- * @src: char pointer
- *
- * Return: int
- */
-
-int get_len(char *src)
-{
-	int len = 0;
-
-	while (*src)
-	{
-		len++;
-		src++;
-	}
-
-	return (len);
-}
-
-/**
  * _memcpy - copy n bytes from memoy area src to dest
  *
  * @dest: char pointer
@@ -33,12 +12,12 @@ int get_len(char *src)
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int len = get_len(dest);
+	/*int len = get_len(dest);*/
 	unsigned int i = 0;
 
 	while (i < n)
 	{
-		dest[len] = src[i];
+		dest[i] = src[i];
 
 		len++;
 		i++;
