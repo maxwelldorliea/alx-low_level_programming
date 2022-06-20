9941ea68ef7430a62078d99602cec9f73d94845d
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-20 13:28:55 -0700

    Bugbfixed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -20,6 +19,6 @@ char *_strchr(char *s, char c)
 		s++;
 	}
 
-	return (NULL);
+	return ('\0');
 }
 
