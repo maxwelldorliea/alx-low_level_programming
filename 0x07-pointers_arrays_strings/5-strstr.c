89b564a9d2e66cdb22848f3f9afcf5616e4eac58
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-20 16:52:27 -0700

    Added file 5 strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * check - check if two string are equal
+ * @s1: char pointer
+ * @s2: char pointer
+ *
+ * Return: int
+ */
+
+int check(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (0);
+		s1++;
+		s2++;
+	}
+
+	return (1);
+}
+
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: char pointer
+ * @needle: char pointer
+ *
+ * Return: char pointer
+ */
+
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		if ((*haystack == *needle) && check(haystack, needle))
+			return (haystack);
+		haystack++;
+	}
+
+	return (NULL);
+}
