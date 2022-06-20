21f3c968747f45a1eb5b1a869afa3ae60e54931a
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-20 06:52:26 -0700

    Added file 1 memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * get_len - return the length of a string
+ *
+ * @src: char pointer
+ *
+ * Return: int
+ */
+
+int get_len(char *src)
+{
+	int len = 0;
+
+	while (*src)
+	{
+		len++;
+		src++;
+	}
+
+	return (len);
+}
+
+/**
+ * _memcpy - copy n bytes from memoy area src to dest
+ *
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: int
+ *
+ * Return: char pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int len = get_len(dest);
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[len] = src[i];
+
+		len++;
+		i++;
+	}
+	dest[len] = '\0';
+
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,4 +4,4 @@ int _putchar(int ch);
 
 char *_memset(char *s, char b, unsigned int n);
 
-
+char *_memcpy(char *dest, char *src, unsigned int n);
