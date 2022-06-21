29631758c393cb1bc42f307915bda2e24fe791d8
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-21 15:57:01 -0700

    Bug fixed

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,19 +13,14 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j,  m1 = 0, m2 = 0;
+	int i, s1 = 0, s2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-	for (j = 0; j <= size; j++)
-	{
-		if (i == j)
-			m1 += a[j];
-		if (i + j == size + 1)
-			m2 += a[j];
+		s1 += *(a + (size * i + i));
+		s2 += *(a + size * i + size - 1 - i);
 	}
 
-	printf("%d, %d\n", m1, m2);
-	}
+	printf("%d, %d\n", s1, s2);
 
 }
