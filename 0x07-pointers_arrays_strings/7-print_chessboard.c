553af565e549f9a9f745715137256ff5604daa28
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-20 17:31:52 -0700

    Added file print chessbord

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print a chessboard
+ *
+ * @a: int pointer to pointer
+ *
+ * Return: Nothing
+ */
+
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j;
+
+	while (i < 8)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+
+}
