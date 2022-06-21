e60b9238c39e4becaf3b5422a9b210a890766806
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-20 18:08:11 -0700

    Added file 100 set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - set value of string a to b
+ *
+ * @s: pointet to pointer
+ * @to: char pointer
+ *
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of two digonal
+ *
+ * @a: int pointer
+ * @size: int
+ *
+ * Return: Nothing
+ */
+
+void print_diagsums(int *a, int size)
