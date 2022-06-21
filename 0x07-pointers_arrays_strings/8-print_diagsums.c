7b71fd626d565948ab54ba0e4848aed831a6e2c7
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-20 19:11:33 -0700

    Added file 8 print digsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 
 /**
  * print_diagsums - prints the sum of two digonal
@@ -10,3 +12,16 @@
  */
 
 void print_diagsums(int *a, int size)
+{
+	int i;
+	long int sum = 0, len = size * size;
+
+	for (i = 0; i < len; i += size + 1)
+		sum += a[i];
+	printf("%li, ", sum);
+
+	for (i = len - 1; i <= len - size + 1; i += size - 1;)
+		sum += a[i];
+	printf("%li\n", sum);
+
+}
