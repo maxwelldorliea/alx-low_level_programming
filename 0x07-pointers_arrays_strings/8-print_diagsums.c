f692fb826411192fee1e20a52617674165c8048c
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-20 19:18:14 -0700

    Error fixed

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,7 +14,8 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	long int sum = 0, len = size * size;
+	long int sum = 0, len;
+	len = size * size;
 
 	for (i = 0; i < len; i += size + 1)
 		sum += a[i];
