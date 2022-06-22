9b2cf5741750ffd1dcf7a253582b8710a1ac12ab
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-21 17:03:50 -0700

    Added file 0 puts recursions

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ *
+ * @s: char pointer
+ *
+ * Return: Nothing
+ */
+
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+
+	_puts_recursion(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,5 @@
+
+
+int _putchar(int ch);
+
+void _puts_recursion(char *s);
