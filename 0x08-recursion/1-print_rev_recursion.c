827dced51cd9ebecf9169d17e5110bae83588af6
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-21 17:49:26 -0700

    Bug fixed

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
-#include <string.h>
+
+
 /**
  * _print_rev_recursion - prints a string in reverse
  *
@@ -10,18 +11,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	static int i = 0;
-	int len = strlen(s) - 1 - i;
-
-	if (*s == '\0')
-		return;
-
-	if (len < 0)
-		return;
-
-	_putchar(s[len]);
-	len--;
-	i++;
-
-	_print_rev_recursion(s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
