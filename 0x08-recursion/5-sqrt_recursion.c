4418101d2870510ccdbbce917f903daa0408e8b7
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-22 01:09:45 -0700

    Added file 5 sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,10 +11,25 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-
-	if (n * n == n)
+	if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt_recursion(n - 1));
+	return (sqr(n, 0));
+}
+
+/**
+ * sqr - return the square root of a number
+ *
+ * @n: int
+ * @cv: int
+ *
+ * Return: int
+ */
+
+int sqr(int n, int cv)
+{
+	if (cv > n / 2)
+		return (-1);
+	if (cv * cv == n)
+		return (cv);
+	return (sqr(n, cv + 1));
 }
