c5e56be52ae0e3eb867d4dc9d2e4ec2f68eefe43
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-22 04:57:33 -0700

    Bug fixed

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,7 +19,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 != *s2 && *s2 != '*')
 		return (0);
 
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0')
 		return (1);
 
 	return (wildcmp(s1 + 1, s2 + 1));
