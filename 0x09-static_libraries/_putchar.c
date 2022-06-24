d982b5cf4563611a6659309e236696896387053d
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-24 16:01:55 -0700

    Error

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * _putchar - prints char to the standard output
+ *
+ * @ch: char
+ *
+ * Return: int
+ */
 
 int _putchar(char ch)
 {
 	extern long write(int, const char *, unsigned long);
 
-	return write(1, &ch, 1);
+	return (write(1, &ch, 1));
 
 }
