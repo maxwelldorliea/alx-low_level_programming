f7345919dd3f8b02046157911d358b58567b07d4
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-24 06:17:40 -0700

    Bug fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program
+ *
+ * @argc: int
+ * @argv: char pointer to pointwr
+*/
 
 
 
 int main(int argc, char* argv[])
 {
-	printf("%s\n", argv[0]);
+	int i;
 
-	return (0);
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	exit(EXIT_SUCCESS);
 }
