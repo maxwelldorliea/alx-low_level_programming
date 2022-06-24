ee8da6871fc380443e4ed738784c17890d118709
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-24 16:31:13 -0700

    Big fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,32 +16,30 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc > 1)
-	{
-	
-	
-
-		for (i = 1; i < argc; i++)
-		{
-			int currv = strtol(argv[i], NULL, 10);
-
-			char *ch = "0";
-
-			if (currv == 0 && *argv[i] != *ch)
-			{
-				printf("Error\n");
-				return (1);
-			}
-
-			sum += currv;
-		}
-
-		printf("%d\n", sum);
-	} 
-	else
+	if (argc == 1)
 	{
 		printf("0\n");
+		exit(EXIT_SUCCESS);
 	}
+	
+	
+
+	for (i = 1; i < argc; i++)
+	{
+		int currv = strtol(argv[i], NULL, 10);
+
+		char *ch = "0";
+
+		if (currv == 0 && *argv[i] != *ch)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += currv;
+	}
+
+	printf("%d\n", sum);
 
 	exit(EXIT_SUCCESS);
 }
