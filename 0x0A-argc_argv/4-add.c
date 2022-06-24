f02a0fc05d7edc09f60f06f1fa51e5f7bfd4817b
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-24 08:40:20 -0700

    Added file 4 add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - prints the sum of int command line argument pass
+ *
+ * @argc: int
+ * @argv: char pointef to pointer
+ *
+ * Return: int
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		exit(EXIT_SUCCESS);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		int currv = strtol(argv[i], NULL, 10);
+
+		char *ch = "0";
+
+		if (currv == 0 && *argv[i] != *ch)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += currv;
+	}
+
+	printf("%d\n", sum);
+
+	exit(EXIT_SUCCESS);
+}
