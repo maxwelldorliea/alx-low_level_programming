d0411e34566e9daa0bbd3429fb3526117d0b3c80
Maxwell D. Dorliea <deboy170@gmail.com>
2022-06-28 05:41:37 -0700

    Bug fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,22 +14,19 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int s1_len, s2_len, t_len, i = 0;
-	char *ans = NULL;
+	int s1_len = strlen(s1), s2_len = strlen(s2), t_len = s1_len + s2_len, i = 0;
+	char *ans = malloc(sizeof(*ans) * t_len + 1);;
 
-	if (*s1 == '\0')
+	if (*s1 == '\0' && *s2 != '\0')
 		return (s2);
-	if (*s2 == '\0')
+	if (*s2 == '\0' && *s1 != '\0')
 		return (s1);
-
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
-	t_len = s1_len + s2_len;
-	ans = malloc(sizeof(*ans) * t_len + 1);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (ans);
 
 	while (i < t_len)
 	{
-		if (i > s1_len)
+		if (i < s1_len)
 			ans[i] = s2[i];
 		ans[i] = s1[i];
 
