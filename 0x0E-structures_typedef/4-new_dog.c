8a0017ca55fd15c85ab1e2fd92a388557511772c
Maxwell D. Dorliea <deboy170@gmail.com>
2022-07-04 15:34:56 -0700

    Added file 4 new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * new_dog - creates a new dog
+ * @name: char pointer
+ * @age: float
+ * @owner: char pointer
+ * Return: Pointer to a new dog
+ */
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int n_len, o_len;
+	char *nname, *nowner;
+
+	dog_t *ndog = malloc(sizeof(*ndog));
+
+	if (ndog == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	if (name == NULL)
+		return (NULL);
+	n_len = strlen(name);
+	nname = malloc(sizeof(*nname) * n_len + 1);
+	if (nname == NULL)
+	{
+		free(nname);
+		return (NULL);
+	}
+	if (owner == NULL)
+		return (NULL);
+	o_len = strlen(owner);
+	nowner = malloc(sizeof(*nowner) * o_len + 1);
+	if (nowner == NULL)
+	{
+		free(nowner);
+		return (NULL);
+	}
+	strcpy(nname, name);
+	ndog->age = age;
+	strcpy(nowner, owner);
+
+	ndog->name = nname;
+	ndog->owner = nowner;
+
+	return (ndog);
+}
