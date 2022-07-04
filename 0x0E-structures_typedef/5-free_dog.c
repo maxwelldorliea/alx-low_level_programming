978c881c95ee2174e8000feafcebe10cc981cb9f
Maxwell D. Dorliea <deboy170@gmail.com>
2022-07-04 15:53:37 -0700

    Added file 5 free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog from the heap
+ * @d: dog_t pointer
+ * Return: Nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,6 +23,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 dog_t *new_dog(char *name, float age, char *owner);
 
+void free_dog(dog_t *d);
+
 void print_dog(struct dog *d);
 
 
