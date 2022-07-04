21ad4a7cd7263a929cfb47e12f73949902ef4642
Maxwell D. Dorliea <deboy170@gmail.com>
2022-07-04 13:52:12 -0700

    Added file dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: char pointer
+ * @age: float
+ * @owner: char pointer
+ *
+ * Description: Define a new type struct dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
