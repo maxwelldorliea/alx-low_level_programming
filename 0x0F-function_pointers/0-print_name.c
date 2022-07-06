e391b0558d02e7b6eb5637ca9b52d13ea640ea56
Maxwell D. Dorliea <deboy170@gmail.com>
2022-07-06 04:54:43 -0700

    Bug Fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -10,7 +11,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
+	if (f == NULL || name == NULL)
 		return;
 	f(name);
 }
