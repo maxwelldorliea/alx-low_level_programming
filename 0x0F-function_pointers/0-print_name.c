0ad73d5f81126a6cec7bf7dec424f479e43624f8
Maxwell D. Dorliea <deboy170@gmail.com>
2022-07-06 04:46:21 -0700

    Added file 0 print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: char pointer
+ * @f: function pointer
+ * Return: Nothing
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
