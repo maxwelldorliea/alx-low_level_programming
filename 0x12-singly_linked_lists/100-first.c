a2ccb7bd663681f7eb73d31c9d4597ccbc36e481
Maxwell D. Dorliea <deboy170@gmail.com>
2022-07-14 14:45:17 -0700

    Added task 5 solution

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * exe_bf_main - prints a string before the main function runs
+ * Return: Nothing
+ */
+
+/*void exe_bf_main(void)__attribute__((constructor))*/
+
+void exe_bf_main(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!");
+printf("\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,4 +28,6 @@ list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
+void exe_bf_main(void)__attribute__((constructor));
+
 #endif
