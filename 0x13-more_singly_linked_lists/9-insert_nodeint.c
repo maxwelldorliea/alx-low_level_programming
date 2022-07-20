bc7294fd0f670e01549e2c53b925f10c73804c67
Maxwell D. Dorliea <deboy170@gmail.com>
2022-07-19 19:35:16 -0700

    Bug fixed in task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,10 +43,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		curr = curr->next;
 		i++;
 	}
-
-	if (idx > i)
-		return (NULL);
-
 	node->next = curr->next;
 	curr->next = node;
 	return (node);
