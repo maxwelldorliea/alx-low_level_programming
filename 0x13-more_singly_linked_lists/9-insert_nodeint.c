0141294ed4322306f0b26700eeed7432431d766d
Maxwell D. Dorliea <deboy170@gmail.com>
2022-07-19 19:32:28 -0700

    Bug fixed in task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (*head);
 	}
 
-	while (i < idx)
+	while (i <= idx)
 	{
 		if (curr == NULL || curr->next == NULL)
 			return (NULL);
