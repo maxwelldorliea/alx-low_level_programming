85e0d050dfe64247e3bdeeaeef94e159bb7ccfd1
Maxwell D. Dorliea <deboy170@gmail.com>
2022-07-21 08:59:22 -0700

    Added task 2 solution

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+/**
+ * get_bit_len - return the number bit in a number
+ * @n: number to be check
+ *
+ * Return: number of bits
+ */
+
+unsigned int get_bit_len(unsigned long int n)
+{
+	int len = 0;
+
+	while (n >> 1)
+	{
+		len++;
+		n >>= 1;
+	}
+
+	return (len);
+}
+
+/**
+ * get_bit - returns a bit at nth index
+ * @n: number to be check
+ * @index: index to check for
+ *
+ * Return: index or -1 if not exist
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int idx = get_bit_len(n);
+	int bit;
+
+	if (index > idx)
+		return (-1);
+
+	bit = (n & (1 << index)) >> index;
+
+	return (bit);
+}
