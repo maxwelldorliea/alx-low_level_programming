57684cd67f18d4f84199a4cf6558a7f79aafb8a3
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-01 23:57:47 +0000

    Added task 7, 8 solution

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+ * insert_dnodeint_at_index - insert at a specify index in a doubly linkedlist
+ * @h: head of the linkedlist
+ * @idx: index to be inserted at
+ * @n: node to be inserted
+ * Return: head of the modified linkedlist
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *node, *tmp;
+	unsigned int index = 0;
+
+	if (!h || !(*h))
+		return (NULL);
+
+	node = malloc(sizeof(*node));
+
+	if (!node)
+	{
+		free(node);
+		node = NULL;
+		return (node);
+	}
+	node->n = n;
+	node->next = NULL;
+	node->prev = NULL;
+
+	tmp = *h;
+
+	while (tmp)
+	{
+		if (idx == index)
+		{
+			node->next = tmp;
+			node->prev = tmp->prev;
+			tmp->prev->next = node;
+			return (*h);
+		}
+		tmp = tmp->next;
+		index++;
+	}
+
+	return (*h);
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+ * delete_dnodeint_at_index - delete node at the specify index
+ * @head: head of the linkedlist
+ * @index: index to be deleted
+ * Return: 1 on Success, -1 on failure
+ */
+
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int idx = 0;
+	dlistint_t *curr, *next;
+
+	if (!head || !(*head))
+		return (-1);
+
+	if (index == idx)
+	{
+		next = (*head)->next;
+		if (next)
+			next->prev = (*head)->prev;
+		free(*head);
+		*head = next;
+		return (1);
+	}
+
+	curr = *head;
+
+	while (curr)
+	{
+
+		if (idx == index)
+		{
+			curr->prev->next = curr->next;
+			curr->next->prev = curr->prev;
+			free(curr);
+
+			return (1);
+		}
+
+		curr = curr->next;
+		idx++;
+	}
+
+	return (-1);
+}
