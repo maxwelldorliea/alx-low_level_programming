969581ae7f3060a90128603cacc1d4f10e684c6f
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-02 01:31:48 +0000

    Added task 12 solution

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == index)
 		return (add_dnodeint(h, n));
 
-	while (tmp)
+	while (tmp->next)
 	{
 		if (idx == index)
 		{
@@ -48,5 +48,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		index++;
 	}
 
-	return (add_dnodeint_end(h, n));
+
+	if (idx == index)
+		return (add_dnodeint_end(h, n));
+
+	return (NULL);
 }
