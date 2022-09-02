6e0f30ed4a2b6f25238cdec7ad82a66781b43413
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-02 02:56:36 +0000

    Fixed bug in task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,12 +12,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *node, *tmp;
-
-	if (!h)
-		return (NULL);
-
-	tmp = *h;
+	dlistint_t *node, *tmp = *h;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
