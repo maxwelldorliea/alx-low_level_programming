1f64ce18f4df3b83bffe9c7f236bbe72658e0ffb
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-05 19:10:11 +0000

    Added task 2 solution

---
+++ 0x18-dynamic_libraries/test/ops.c
@@ -0,0 +1,72 @@
+#include "ops.h"
+
+
+
+/**
+ * add - return the sum of two numbers
+ * @a: value to be added
+ * @b: value to be added
+ * Return: the sum of a and b
+ */
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - return the difference of two numbers
+ * @a: value to be added
+ * @b: value to be added
+ * Return: the difference of a and b
+ */
+
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - return the prod of two numbers
+ * @a: value to be added
+ * @b: value to be added
+ * Return: the prod of a and b
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * div - return the quotient of two numbers
+ * @a: value to be added
+ * @b: value to be added
+ * Return: quotient of a and b
+ */
+
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+
+/**
+ * mod - return the remainder of two numbers
+ * @a: value to be added
+ * @b: value to be added
+ * Return: remainder of a and b
+ */
+
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
