b3e10f321339920894112287bc1a8d4589c7688a
Maxwell D. Dorliea <deboy170@gmail.com>
2023-01-02 21:40:08 +0000

    Added task 0 solution

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search for a target in an array
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: target to search for 
+ * Return: index of the first occurrance of the target or -1 if not found
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
